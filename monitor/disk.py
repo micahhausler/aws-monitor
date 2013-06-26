0135f74dfd1eb732c4f9c3b1633570c9fda9a3d0
micahhausler <micah.hausler@akimbo.io>
2013-06-26 14:21:49 -0400

    updated metric names

--- monitor/disk.py
+++ monitor/disk.py
@@ -8,8 +8,12 @@ def disk_stats():
     mountpoints = [disk.mountpoint for disk in disk_partitions()]
     for mp in mountpoints:
         usage[mp] = {}
-        usage[mp]['percent'] = disk_usage(mp).percent
-        usage[mp]['total'] = disk_usage(mp).total
-        usage[mp]['used'] = disk_usage(mp).used
-        usage[mp]['free'] = disk_usage(mp).free
-    return usage
+        usage[mp]['disk_percent'] = disk_usage(mp).percent
+        usage[mp]['disk_total'] = disk_usage(mp).total
+        usage[mp]['disk_used'] = disk_usage(mp).used
+        usage[mp]['disk_free'] = disk_usage(mp).free
+
+    ret = {}
+    for k, v in usage.iteritems():
+        ret["disk " + k] = v
+    return ret
