d36ee95e8af367294f26c85ff05e8c8a2f0bfb08
micahhausler <micah.hausler@akimbo.io>
2013-06-26 15:15:43 -0400

    modified setup

--- setup.py
+++ setup.py
@@ -4,6 +4,17 @@
 import os
 from setuptools import setup
 
+try:
+    from setuptools import setup, find_packages
+
+    # Silence pyflakes
+    assert setup
+    assert find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
@@ -14,28 +25,31 @@ def read(fname):
 
 os.system("pip install -r requirements.txt")
 
+VERSION = read(os.path.join('aws_monitor', 'VERSION')).strip()
+
+install_requires = [
+        l for l in read('requirements.txt').split('\n')
+        if l and not l.startswith('#')]
+
 setup(
     name="aws-monitor",
-    version="0.0.1",
+    version=VERSION,
+    packages=find_packages(),
     author="Micah Hausler",
     author_email="micah.hausler@akimbo.io",
     description=(
         "An Amazon Web services system library that is used at Akimbo LLC"),
-    license="Closed",
-    keywords="AWS",
+    license="BSD",
+    keywords="AWS CloudWatch monitoring",
     url="http://getfireplug.com",
     long_description=read('README.md'),
     dependency_links = [],
-    install_requires=[
-        "requests==1.2.0",
-        "boto==2.9.4",
-        "psutil==0.7.1",
-    ],
+    install_requires=install_requires,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Topic :: System :: Monitoring",
         "License :: OSI Approved :: Closed",
     ],
-    package_data={'aws-monitor': ['VERSION']},
-    entry_points={'console_scripts': ['aws-monitor=aws-monitor.push:main']}
+    package_data={'aws_monitor': ['VERSION']},
+    entry_points={'console_scripts': ['aws-monitor=aws_monitor.push:main']}
 )
